75325e9b1b453be9e58ae8571d2c4c7d198499ae
树佬呀 <264035699@qq.com>
2022-09-12 21:10:42 +0800

    update port

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ const flatListData = require('./src/mock/bookFlatList')
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
   devServer: {
-    port: 7891,
+    port: 5432,
     before(app) {
       mock(app, '/book/home', homeData)
       mock(app, '/book/shelf', shelfData)
